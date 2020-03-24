970523feabee704863316c24f7988f9b675b26ec
cho881020 <kj_cho@nepp.kr>
2020-03-24 16:37:32 +0900

    어댑터 생성, 리스트뷰와 연결  => 서버에서 받아온 데이터 새로고침

---
+++ app/src/main/java/kr/co/tjoeun/a20200323_01_loginandsignup/adapters/BlackAdapter.java
@@ -0,0 +1,44 @@
+package kr.co.tjoeun.a20200323_01_loginandsignup.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+import kr.co.tjoeun.a20200323_01_loginandsignup.R;
+import kr.co.tjoeun.a20200323_01_loginandsignup.datas.Black;
+
+public class BlackAdapter extends ArrayAdapter<Black> {
+
+    Context mContext;
+    List<Black> mList;
+    LayoutInflater inf;
+
+    public BlackAdapter(@NonNull Context context, int resource, @NonNull List<Black> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.black_list_item, null);
+        }
+
+        return row;
+
+    }
+}
